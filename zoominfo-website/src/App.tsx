6d7f6cddf6055b5a8905359c9a439b0a06f439fe
Ankit Kumar <ankits.kvmdam@hotmail.com>
2024-10-17 00:49:39 +0900

    fix focus

--- zoominfo-website/src/App.tsx
+++ zoominfo-website/src/App.tsx
@@ -1,16 +1,37 @@
-import { useState } from "react";
+import React from "react";
 import "./App.css";
 
 function App() {
-  const [isOpen, setOpen] = useState(0);
+  const ref = React.useRef<HTMLIFrameElement>(null);
 
+  React.useEffect(() => {
+    function onMessage(event: MessageEvent) {
+      if (event.data === "space" && ref.current) {
+        ref.current.focus();
+        ref.current.contentWindow?.focus();
+      }
+    }
+
+    window.addEventListener("message", onMessage);
+
+    return () => {
+      window.removeEventListener("message", onMessage);
+    };
+  }, []);
   return (
     <div className="container">
-      <div className="zoominfo-container">
+      <div
+        onClick={() => console.log("You clicked!!!!!")}
+        className="zoominfo-container"
+      >
         <iframe src="/zoom.htm" />
       </div>
       <div className="widget-container">
-        <iframe src="http://localhost:5173" allow="microphone;camera" />
+        <iframe
+          ref={ref}
+          src="http://localhost:5173/?functionURL=https://us0-dev.outspeed.com/run/333fede65db918130ca947074ddb3a43"
+          allow="microphone;camera"
+        />
       </div>
     </div>
   );
